a756cca338008cc4f678441f319e971241ceaf0b
wangshuqin <shuqin.wang@newtouch.com>
2023-02-20 16:13:19 +0800

    login-test

--- src/views/login/loginTransition.vue
+++ src/views/login/loginTransition.vue
@@ -79,7 +79,7 @@ onMounted(async () => {
       const state = new Date().getTime();
       const oauthUrl = ref('https://github.com/apps/hamster-test/installations/new');
       const url = `${oauthUrl.value}?state=${state}`;
-      const myWindow = window.open(url, 'login-github', 'modal=yes,toolbar=no,titlebar=no,menuba=no,location=no,top=100,left=500,width=800,height=700')
+      const myWindow = window.open(url, '_parent', 'modal=yes,toolbar=no,titlebar=no,menuba=no,location=no,top=100,left=500,width=800,height=700')
     } else {
       code.value = router.currentRoute.value.query?.code || '';
       if (code.value) {
