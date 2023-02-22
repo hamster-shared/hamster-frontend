30907d3a4d85472b686232065d4e1fd3fed1491d
leary423 <leary620@163.com>
2023-02-22 15:42:09 +0800

    fix contractList

--- src/views/projects/projectsWorkflows/components/ContractList.vue
+++ src/views/projects/projectsWorkflows/components/ContractList.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="contracyList p-[32px] dark:bg-[#1D1C1A] bg-[#ffffff] dark:text-white text-[#121211] rounded-[12px] mt-[32px]">
+    class="contracyList p-[32px] dark:bg-[#1D1C1A] bg-[#ffffff] dark:text-white text-[#121211] rounded-[12px] my-[32px]">
     <div class="flex justify-between mb-[32px]">
       <span class="text-[24px] font-bold">{{ $t("workFlows.contractList") }}</span>
       <a-button class="btn" @click="toDeployUrl(contractListData[0])" :disabled="contractListData?.length <= 0">{{
