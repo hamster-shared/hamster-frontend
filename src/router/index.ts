bc41b0d1fde15eaf29387df2c725ead3f9ed40f1
Dennisic\Chen <cgyforwork@foxmail.com>
2023-03-02 14:54:52 +0800

    feat: 暂存develop分支改动代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/login/index.vue";
-import LoginTransition from "../views/login/loginTransition.vue";
-import Welcome from "../views/login/welcome.vue";
-import HomeView from "../views/home/HomeView.vue";
-import RpcsIndex from "../views/nodeService/rpcs/index.vue";
-import AppsIndex from "../views/nodeService/apps/index.vue";
-import ProjectsList from "../views/projects/projectsList/index.vue";
-import ProjectsListDetails from "../views/projects/projectsListDetails/index.vue";
-import ProjectsCreat from "../views/projects/projectsCreat/index.vue";
-import ProjectsTemplate from "../views/projects/projectsTemplate/index.vue";
-import ProjectsTemplatesDetails from "../views/projects/projectsTemplatesDetails/index.vue";
-import ProjectsWorkflows from "../views/projects/projectsWorkflows/index.vue";
-import ProjectsWorkflowsAllLogs from "../views/projects/projectsWorkflowsAllLogs/index.vue";
-import projectsArtifactsContractDeploy from "../views/projects/projectsArtifactsContractDeploy/index.vue";
-import ProjectsContractsDetails from "../views/projects/projectsContractsDetails/index.vue";
-import ProjectsStandardContract from "../views/projects/projectsStandardContract/index.vue";
-import ProjectsFrontendDetails from "../views/projects/projectsFrontendDetails/index.vue";
-import ProjectsIntegrated from "../views/projects/projectsIntegrated/index.vue";
+// import Login from "../views/login/index.vue";
+// import LoginTransition from "../views/login/loginTransition.vue";
+// import Welcome from "../views/login/welcome.vue";
+// import HomeView from "../views/home/HomeView.vue";
+// import RpcsIndex from "../views/nodeService/rpcs/index.vue";
+// import AppsIndex from "../views/nodeService/apps/index.vue";
+// import ProjectsList from "../views/projects/projectsList/index.vue";
+// import ProjectsListDetails from "../views/projects/projectsListDetails/index.vue";
+// import ProjectsCreat from "../views/projects/projectsCreat/index.vue";
+// import ProjectsTemplate from "../views/projects/projectsTemplate/index.vue";
+// import ProjectsTemplatesDetails from "../views/projects/projectsTemplatesDetails/index.vue";
+// import ProjectsWorkflows from "../views/projects/projectsWorkflows/index.vue";
+// import ProjectsWorkflowsAllLogs from "../views/projects/projectsWorkflowsAllLogs/index.vue";
+// import projectsArtifactsContractDeploy from "../views/projects/projectsArtifactsContractDeploy/index.vue";
+// import ProjectsContractsDetails from "../views/projects/projectsContractsDetails/index.vue";
+// import ProjectsStandardContract from "../views/projects/projectsStandardContract/index.vue";
+// import ProjectsFrontendDetails from "../views/projects/projectsFrontendDetails/index.vue";
+// import ProjectsIntegrated from "../views/projects/projectsIntegrated/index.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/home",
       name: "home",
-      component: HomeView,
+      // component: HomeView,
+      component: () => import('@/views/home/HomeView.vue'),
     },
     // {
     //   path: "/",
@@ -46,7 +47,7 @@ const router = createRouter({
         {
           path: '/login',
           name: 'Login',
-          component: Login,
+          component: import('@/views/login/index.vue'),
         }
       ],
       meta: {
@@ -56,7 +57,7 @@ const router = createRouter({
     {
       path: '/loginTransition',
       name: 'LoginTransition',
-      component: LoginTransition,
+      component: import('@/views/login/loginTransition.vue'),
       meta: {
         layout: 'null',
       }
@@ -64,7 +65,7 @@ const router = createRouter({
     {
       path: '/welcome',
       name: 'Welcome',
-      component: Welcome,
+      component: import('@/views/login/welcome.vue'),
       meta: {
         layout: 'null',
       }
@@ -79,24 +80,24 @@ const router = createRouter({
             {
               path: "/projects",
               name: "ProjectsList",
-              component: ProjectsList,
+              component: import('@/views/projects/projectsList/index.vue'),
             },
             {
               path: "/projects/:id/details/:type",
               name: "ProjectsListDetails",
-              component: ProjectsListDetails,
+              component: import('@/views/projects/projectsListDetails/index.vue'),
             },
             {
               path: "/projects/integrated/:id",
               name: "ProjectsIntegrated",
-              component: ProjectsIntegrated
+              component: import('@/views/projects/projectsIntegrated/index.vue')
             }
           ],
         },
         {
           path: "/projects/create",
           name: "ProjectsCreat",
-          component: ProjectsCreat,
+          component: import('@/views/projects/projectsCreat/index.vue'),
         },
         {
           path: "/projects/template/:type",
@@ -105,17 +106,17 @@ const router = createRouter({
             {
               path: "/projects/template/:type",
               name: "ProjectsTemplate",
-              component: ProjectsTemplate,
+              component: import('@/views/projects/projectsTemplate/index.vue'),
             },
             {
               path: "/projects/templates/:templateId/details/:type",
               name: "ProjectsTemplatesDetails",
-              component: ProjectsTemplatesDetails,
+              component: import('@/views/projects/projectsTemplatesDetails/index.vue'),
             },
             {
               path: "/projects/templates/:contractName/standard",
               name: "ProjectsStandardContract",
-              component: ProjectsStandardContract,
+              component: import('@/views/projects/projectsStandardContract/index.vue'),
             },
           ]
         },
@@ -126,12 +127,12 @@ const router = createRouter({
             {
               path: "/projects/:id/:workflowsId/workflows/:workflowDetailId/:type/:projectType",
               name: "ProjectsWorkflows",
-              component: ProjectsWorkflows,
+              component: import('@/views/projects/projectsWorkflows/index.vue'),
             },
             {
               path: "/projects/:workflowsId/workflows/:workflowDetailId/allLogs",
               name: "ProjectsWorkflowsAllLogs",
-              component: ProjectsWorkflowsAllLogs,
+              component: import('@/views/projects/projectsWorkflowsAllLogs/index.vue'),
               meta: {
                 layout: 'null',
               }
@@ -141,17 +142,17 @@ const router = createRouter({
         {
           path: "/projects/:id/artifacts-contract/:version/deploy/:contract",
           name: "projectsArtifactsContractDeploy",
-          component: projectsArtifactsContractDeploy,
+          component: import('@/views/projects/projectsArtifactsContractDeploy/index.vue'),
         },
         {
           path: "/projects/:id/contracts-details/:version",
           name: "ProjectsContractsDetails",
-          component: ProjectsContractsDetails,
+          component: import('@/views/projects/projectsContractsDetails/index.vue'),
         },
         {
           path: "/projects/:workflowsId/frontend-details/:workflowDetailId/:packageId",
           name: "projectsFrontendDetails",
-          component: ProjectsFrontendDetails,
+          component: import('@/views/projects/projectsFrontendDetails/index.vue'),
         }
       ]
     },
@@ -162,12 +163,12 @@ const router = createRouter({
         {
           path: "/node-service/RPCs",
           name: "RPCs",
-          component: RpcsIndex,
+          component: import('@/views/nodeService/rpcs/index.vue'),
         },
         {
           path: "/node-service/Apps",
           name: "AppsIndex",
-          component: AppsIndex,
+          component: import('@/views/nodeService/apps/index.vue'),
         }
       ],
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig, loadEnv } from "vite";
+import type {ConfigEnv} from 'vite';
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => defineConfig({
+export default ({ mode }:ConfigEnv) => defineConfig({
   build: {
   },
   plugins: [vue()],
