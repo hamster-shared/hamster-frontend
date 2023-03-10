03663a4ed8c45c3bf4a1faee773ba9af166a2828
mohaijiang <mohaijiang110@163.com>
2023-03-10 21:14:49 +0800

    Revert "homePage optimizing 2"
    This reverts commit 58a65300

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,17 @@ import path from 'path';
 import { defineConfig, loadEnv } from "vite";
 import type {ConfigEnv} from 'vite';
 import vue from "@vitejs/plugin-vue";
-import viteCompression from 'vite-plugin-compression';
-import OptimizationPersist from 'vite-plugin-optimize-persist';
-import PkgConfig from 'vite-plugin-package-config';
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default ({ mode }:ConfigEnv) => defineConfig({
   build: {
-    outDir: 'build', // 打包文件的输出目录
-    assetsDir: 'static', // 静态资源的存放目录
-    assetsInlineLimit: 4096, // 图片转 base64 编码的阈值
-    minify: 'terser', // 混淆器，terser构建后文件体积更小
   },
   plugins: [
     vue(),
     viteCompression({
       // threshold: 1024000 // 对大于 1mb 的文件进行压缩
-    }),
-    OptimizationPersist(),//预构建
-    PkgConfig(),//从包依赖中读取预构建项
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
-      // 指定symbolId格式
-      symbolId: "icon-[dir]-[name]",
-    }),
-  ],
+    })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -53,10 +37,5 @@ export default ({ mode }:ConfigEnv) => defineConfig({
         rewrite: (path) => path.replace(/^\/nodeService/, ""),
       },
     },
-    host:true,//同一局域网可以访问本地服务
   },
-  // 强制预构建插件包
-  // optimizeDeps: {
-  //   include: ['axios'],
-  // },
 });
