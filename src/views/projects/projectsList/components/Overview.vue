0b1b3bc3f26aca0a86e4284a1a387db9a707104d
yulia <yuliayqy@gmail.com>
2023-03-23 10:39:25 +0800

    about aptos build

--- src/views/projects/projectsList/components/Overview.vue
+++ src/views/projects/projectsList/components/Overview.vue
@@ -164,7 +164,7 @@
   <CustomMsg :showMsg="showMsg" :msgType="msgType" :msgParam="msgParam"></CustomMsg>
   <starkNetModal :starknetVisible="starknetVisible" :deployTxHash="deployTxHash" @cancelModal="starknetVisible = false">
   </starkNetModal>
-  <AptosBuildParams :aptosBuildVisible="aptosBuildVisible" :detailId="viewInfo?.id" :aptosBuildParams="aptosBuildParams" @hideAptosBuildVisible="hideAptosBuildVisible" @aptosBuild="aptosBuild"/>
+  <AptosBuildParams :aptosBuildVisible="aptosBuildVisible" :detailId="viewInfo?.id" v-model:aptosBuildParams="aptosBuildParams" @hideAptosBuildVisible="hideAptosBuildVisible" @aptosBuild="aptosBuild"/>
 </template>
 <script lang='ts' setup>
 import { ref, toRefs, computed, reactive } from 'vue';
@@ -298,15 +298,6 @@ const buildStatusAction = async (id: string, buildData: any)=> {
   }
 }
 
-const checkAptosWalletInstalled = () => {
-  if ('aptos' in window) {
-    aptosBuildVisible.value = true
-    return window.aptos;
-  } else {
-    window.open('https://petra.app/', `_blank`);
-  }
-};
-
 const aptosBuildParams = ref([])
 const projectsBuild = async (id: string, buildData: any, frameType:string) => {
   console.log('projectsBuild:::', id, buildData.status, frameType, projectType.value)
@@ -314,23 +305,22 @@ const projectsBuild = async (id: string, buildData: any, frameType:string) => {
   const needsParams = res.data.needsParams
   try {
     if (frameType == '2' && needsParams) {
-      await checkAptosWalletInstalled()
+      aptosBuildVisible.value = true
       const { data } = await apiGetAptosBuildParams(id)
       console.log('apiGetAptosBuildParams:::', data)
       aptosBuildParams.value = data
     }else if (frameType == '2' && !needsParams){
-      // if (buildData.status == 1){
-      //   message.info("Executing Now，please wait a moment.");
-      // } else {
+      if (buildData.status == 1){
+        message.info("Executing Now，please wait a moment.");
+      } else {
+        const { data } = await apiAptosBuild(id)
+        msgParam.value.workflowsId = data.workflowId;
+        msgParam.value.workflowDetailId = data.id;
+        msgType.value = 'build';
+        setMsgShow();
 
-      // }
-      const { data } = await apiAptosBuild(id)
-      msgParam.value.workflowsId = data.workflowId;
-      msgParam.value.workflowDetailId = data.id;
-      msgType.value = 'build';
-      setMsgShow();
-
-      loadView();
+        loadView();
+      }
     }else {
       buildStatusAction(id, buildData)
     }
@@ -443,7 +433,7 @@ const frontendDeploying = async () => {
 const hideAptosBuildVisible = () =>{
   aptosBuildVisible.value = false
 }
-const aptosBuild = async(id:any)=>{
+const aptosBuild = async(id:any, formData:any)=>{
   try {
     const { data } = await apiAptosBuild(id.value)
     console.log('aptosbuild::',data)
