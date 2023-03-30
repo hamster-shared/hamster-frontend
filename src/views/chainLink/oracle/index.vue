676ba5f4f4085e150f2bc16f9dbf914d337b57f1
yulia <yuliayqy@gmail.com>
2023-03-30 11:15:29 +0800

    about chainlink change

---
+++ src/views/chainLink/oracle/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>chainlinkFun</div>
+  <rpcAndOracleChart></rpcAndOracleChart>
+</template>
+
+<script setup lang="ts">
+  import rpcAndOracleChart from '../components/rpcAndOracleChart.vue'
+</script>
