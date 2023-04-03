7e2f4e6bfbfa410bbb4a5dc075e63a53f8db257d
wangshuqin <shuqin.wang@newtouch.com>
2023-04-03 18:21:50 +0800

    page name

--- src/views/chainLink/components/rpcAndOracleDefault.vue
+++ src/views/chainLink/components/rpcAndOracleDefault.vue
@@ -1,29 +1,30 @@
 <template>
-  <div v-if="type == 'rpc'" class="mt-12">
+  <div v-if="type == 'RPC'" class="mt-12">
     <div class="relative text-center">
-      <img src="@/assets/images/Rpc.png" class="w-[300px]"/>
+      <img src="@/assets/images/Rpc.png" class="w-[300px]" />
       <div class="absolute text-4xl font-bold left-[47%] bottom-[12px]">RPC</div>
     </div>
     <div class="text-center">
-      <span class="block w-[80%] mx-auto mt-2 mb-4">Web3 development is inseparable from the support of blockchain nodes, 
-        and building and maintaining nodes requires a lot of cost and effort. 
-        In order to enable developers to obtain efficient and stable node services, 
+      <span class="block w-[80%] mx-auto mt-2 mb-4">Web3 development is inseparable from the support of blockchain nodes,
+        and building and maintaining nodes requires a lot of cost and effort.
+        In order to enable developers to obtain efficient and stable node services,
         Miwago provides RPC services to meet the development needs of developers for various ecological projects .
       </span>
       <a-button @click="router.push('/chainlink/RPC')">Get service now</a-button>
     </div>
   </div>
 
-  <div v-if="type == 'oracle'" class="mt-12">
+  <div v-if="type == 'Oracle'" class="mt-12">
     <div class="text-center">
-      <img src="@/assets/images/oracle.png" class="w-[300px]"/>
+      <img src="@/assets/images/oracle.png" class="w-[300px]" />
       <div class="text-4xl font-bold">Oracle</div>
     </div>
     <div class="text-center">
-      <span class="block w-[80%] mx-auto mt-2 mb-4">The Oracle is the messenger for the interaction between the blockchain and the real world data. 
-        It is an indispensable infrastructure for the web3 ecology, 
-        and its development plays a vital role in the prosperity of the web3 ecology. 
-        In order to allow developers to quickly access more suitable oracle services with zero code or low code, 
+      <span class="block w-[80%] mx-auto mt-2 mb-4">The Oracle is the messenger for the interaction between the blockchain
+        and the real world data.
+        It is an indispensable infrastructure for the web3 ecology,
+        and its development plays a vital role in the prosperity of the web3 ecology.
+        In order to allow developers to quickly access more suitable oracle services with zero code or low code,
         the Hamster platform aggregates various oracle solution middleware on the market.
       </span>
       <a-button @click="router.push('/chainlink/oracle')">Get service now</a-button>
@@ -32,15 +33,13 @@
 </template>
 
 <script setup lang="ts">
-import { useRoute,useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
-  const routes = useRoute();
-  const router = useRouter();
+const routes = useRoute();
+const router = useRouter();
 
-  const type = routes.params.type
+const type = routes.params.type
 
 </script>
 
-<style scoped lang="less">
-  
-</style>
+<style scoped lang="less"></style>
