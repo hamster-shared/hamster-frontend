ed0a8c0f394fcdd55dda227c1ad64c12732a3490
Dennisic\Chen <cgyforwork@foxmail.com>
2023-06-15 11:27:43 +0800

    feat: save code

--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -116,14 +116,14 @@ const createQRcode = () => {
 }
 // 倒计时
 const countTime = () => {
-  let duration:any = 10
+  let duration:any = 3600
   timeId.value = setInterval(() => {
     --duration
     time.value = formatTimeCallback(duration)
     console.log('倒计时',duration,time.value)
     if(duration<1){
       clearInterval(timeId.value)
-      status.value = 4
+      status.value = 3
     }
   }, 1000);
 }
