29b83b4f315c6aa43d508e8cedc42d6985aea61a
leary423 <leary620@163.com>
2023-07-04 16:49:51 +0800

    fix bug 929

--- src/components/CustomMsg.vue
+++ src/components/CustomMsg.vue
@@ -5,8 +5,11 @@
         <div class="message-custom-content bg-[#FFF9F2] dark:bg-[#22190E] text-[#000000] dark:text-[#FFB45B] ">
           <div class="ant-message-success">
             <span role="img" aria-label="check-circle" class="anticon anticon-check-circle"><svg focusable="false" class="" data-icon="check-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"></path></svg></span>
-            <span v-if="msgType === 'deploy'">The workflow of deployment is running, <a :href="`/projects/${msgParam.id}/${msgParam.workflowsId}/workflows/${msgParam.workflowDetailId}/3/${msgParam.projectType}`">view now</a></span>
-            <span v-else-if="msgType === 'build'">The workflow of building is running, <a :href="`/projects/${msgParam.id}/${msgParam.workflowsId}/workflows/${msgParam.workflowDetailId}/2/${msgParam.projectType}`">view now</a></span>
+            <span v-if="msgType === 'deploy'">The workflow of deployment is running, </span>
+            <span v-else-if="msgType === 'build'">The workflow of building is running, </span>
+            <span v-else-if="msgType === 'check'">The workflow of checking is running, </span>
+            <a :href="`/projects/${msgParam.id}/${msgParam.workflowsId}/workflows/${msgParam.workflowDetailId}/${msgParam.operateType}/${msgParam.projectType}`">view now</a>
+
           </div>
         </div>
       </div>
