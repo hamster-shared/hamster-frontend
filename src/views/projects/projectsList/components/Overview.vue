6c8b210cb3ed513cc89e5709fc09ece52448a792
leary423 <leary620@163.com>
2023-07-26 09:53:14 +0800

    add deploy ic

--- src/views/projects/projectsList/components/Overview.vue
+++ src/views/projects/projectsList/components/Overview.vue
@@ -504,8 +504,12 @@ const projectsDeploy = async (id: string, version: string, status: Number) => {
       // 前端并且是ipfs的时候
       if(viewInfo.value.type == '2' && viewInfo.value.deployType==1){
         message.info("Package not avaliable");
-      }else{
-        message.info("Project image not avaliable");
+      } else {
+        if (viewInfo.value.deployType === 3) {
+          showDeployIC.value = true;
+        } else {
+          message.info("Project image not avaliable");
+        }
       }
     }
   }
