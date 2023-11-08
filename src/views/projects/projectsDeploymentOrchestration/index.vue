14d588ae3ee4a44c107d02329087dcb6f677ef65
leary423 <leary620@163.com>
2023-11-08 12:19:46 +0800

    change contract number

--- src/views/projects/projectsDeploymentOrchestration/index.vue
+++ src/views/projects/projectsDeploymentOrchestration/index.vue
@@ -620,7 +620,6 @@ const deployManyContract = async () => {
     if (noSaveContract.value.length == 0) {
       // 保存编排信息
       await saveOrchestrationInfo();
-      numberValue.value = contractOrchestration.value.length;
       
       // 部署调用代码
       visibleNumber.value = true
@@ -687,8 +686,10 @@ const getArrangeDeployList = async () => {
     res.data.forEach((item: any) => {
       if (item != '') {
         let strList = JSON.parse(item);
+        console.log("strList:",strList);
         strList.deployStep.forEach((sub: any) => {
           deployStep.push(sub);
+          numberValue.value += sub.steps.length - 1; // method数量累加
           if (sub.contract.proxy) {
             let copyData = JSON.parse(JSON.stringify(sub));
             copyData.steps.length = 1; // 只需要copy一条数据
@@ -730,6 +731,7 @@ const getArrangeDeployList = async () => {
         }
       }
     });
+    numberValue.value += deployStep.length; //合约数量累加
     deployArrange.value = {
       deployStep: deployStep,
       step: 0,
