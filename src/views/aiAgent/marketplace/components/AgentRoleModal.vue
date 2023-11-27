853955b28d03f030cef8c2b82d590263921642ec
leary423 <leary620@163.com>
2023-11-27 12:17:31 +0800

    add marketplace

---
+++ src/views/aiAgent/marketplace/components/AgentRoleModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <a-modal :footer="null" width="760px" centered="true" v-model:visible="modalVisible">
+    <div class="text-[24px] text-[#151210] font-bold">Please select the Agent role style</div>
+    <template #closeIcon>
+      <img class="mt-5" src="@/assets/icons/closeIcon.svg" @click="hideVisible" />
+    </template>
+    <div class="my-[50px]">
+      <a-radio-group v-model:value="roleValue">
+        <a-radio-button v-for="(item,key) in roleList" :key="key" :value="item.value">{{ item.label }}</a-radio-button>
+      </a-radio-group>
+    </div>
+    <div class="text-center">
+      <a-button type="primary" class="w-[240px] !h-[43px] mr-[24px]" ghost @click="hideVisible">Skip</a-button>
+      <a-button type="primary" class="w-[240px] !h-[43px]" @click="handleOk">Confirm</a-button>
+    </div>
+  </a-modal>
+</template>
+<script setup lang="ts">
+import { ref, toRefs } from 'vue';
+
+const props = defineProps({
+  modalVisible: Boolean,
+});
+const { modalVisible } = toRefs(props);
+const emits = defineEmits(['hiddenModal']);
+
+const roleValue = ref('');
+const roleList = ref([{ label:'御姐之魅', value:'御姐之魅'},{ label:'高贵公主', value:'高贵公主'},{ label:'杠精之道', value:'杠精之道'},{ label:'温柔甜心', value:'温柔甜心'},{ label:'王者气息', value:'王者气息'},{ label:'机智灵狐', value:'机智灵狐'},{ label:'可爱萌物', value:'可爱萌物'},{ label:'冰山美人', value:'冰山美人'},{ label:'热情狂烈', value:'热情狂烈'}]);
+
+const handleOk = async () => {
+  
+  hideVisible();
+}
+const hideVisible = () => {
+  emits('hiddenModal');
+}
+</script>
+<style scoped lang="less">
+:deep(.ant-radio-group){
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+}
+:deep(.ant-radio-group .ant-radio-button-wrapper){
+  width: 218px;
+  height: 62px;
+  line-height: 60px;
+  background: #FFFFFF;
+  border-radius: 31px;
+  border: 2px solid #DCDCDC;
+  color: #696D71;
+  text-align: center;
+}
+:deep(.ant-radio-button-wrapper-checked:not(.ant-radio-button-wrapper-disabled)){
+  border-color: #E2B578;
+  color: #000000;
+}
+:deep(.ant-radio-button-wrapper:not(:first-child):before){
+  display: none;
+}
+</style>
--- src/views/aiAgent/marketplace/detail.vue
+++ src/views/aiAgent/marketplace/detail.vue
@@ -1,24 +1,31 @@
 <template>
   <div :class="theme.themeValue === 'dark' ? 'dark-css' : ''">
     <div class="font-bold text-[24px] mb-[30px]">Marketplace</div>
-    <div class="bg-[#F5F6F8] rounded-[12px]">
-      <div class="bg-[#EBEBEC] rounded-tl-[12px] rounded-tr-[12px] p-[30px] flex justify-between items-center">
+    <div class="bg-[#F5F6F8] dark:bg-[#0E0E0E] rounded-[12px] pb-[20px]">
+      <div class="bg-[#EBEBEC] dark:bg-[#161616] rounded-tl-[12px] rounded-tr-[12px] p-[30px] flex justify-between items-center">
         <div class="flex justify-center">
           <img :src="getImageURL(`${detailInfo.logo}`)" class="h-[100px] w-[100px] rounded-full mr-[20px]"/>
           <div class="font-semibold text-[#656D7A]">
-            <div><label class="text-[21px] text-[#000000] mr-[10px]">{{ detailInfo.title }}</label><label class="text-[16px]">{{ detailInfo.name }}</label></div>
+            <div><label class="text-[21px] text-[#000000] dark:text-[#FFFFFF] mr-[10px]">{{ detailInfo.title }}</label><label class="text-[16px]">{{ detailInfo.name }}</label></div>
             <div class="text-[14px] mt-[7px] text-ellipsis desc-w">{{ detailInfo.desc }}</div>
           </div>
         </div>
-        <a-button type="primary" >Continue Working</a-button>
+        <div>
+          <a-button class="mr-[20px]" type="primary" @click="hireAgent">Hire agent and start working</a-button>
+          <a-button type="primary" @click="cloneAgent">Clone Agent</a-button>
+          <a-button v-if="false" type="primary" @click="continueWork">Continue Working</a-button>
+        </div>
       </div>
+      <div class="bg-[#EBEBEC] dark:bg-[#161616] rounded-[12px] m-[20px] mb-0 p-[20px] max-h-[635px] overflow-y-auto text-[14px] font-light" v-html="detailInfo.content.replace(/\n/g, '<br>')"></div>
     </div>
   </div>
+  <AgentRoleModal :modalVisible="modalVisible" @hiddenModal="modalVisible=false"></AgentRoleModal>
 </template>
 <script setup lang="ts">
-import { toRefs } from "vue";
+import { ref, toRefs } from "vue";
 import useAssets from "@/stores/useAssets";
 import { useThemeStore } from "@/stores/useTheme";
+import AgentRoleModal from "./components/AgentRoleModal.vue";
 
 const { getImageURL } = useAssets();
 const theme = useThemeStore();
@@ -28,8 +35,22 @@ const props = defineProps({
     default: () => '',
   },
 });
+const modalVisible = ref(false);
 const { id } = toRefs(props);
-const detailInfo = { id: '1', logo: 'testLogo.png', name: '币圈索罗斯', title: '投资顾问', desc: '项目研报、Alpha资源、钱包监控、自动min挖掘…项目研报、Alpha资源、钱包监控、自动min挖掘…项目研报、Alpha资源、钱包监控、自动…' };
+const detailInfo = {
+  id: '1', logo: 'testLogo.png', name: '币圈索罗斯', title: '投资顾问', desc: '项目研报、Alpha资源、钱包监控、自动min挖掘…项目研报、Alpha资源、钱包监控、自动min挖掘…项目研报、Alpha资源、钱包监控、自动…',
+  content: "Are you looking for a trusted Investment Advisor who can help you make informed decisions and optimize your investment portfolio?<br><br>I am an experienced Investment Consultant dedicated to providing personalized investment advice and strategies tailored to your unique financial goals and risk tolerance. With a deep understanding of the financial markets and a passion for helping clients achieve their investment objectives, I offer comprehensive services to support your investment journey.<br><br>Here's how I can assist you:<br><br>🔍 Risk Assessment: By assessing your risk profile, analyzing your financial information, investment goals, and risk tolerance, I can provide you with a detailed risk assessment report. This report will include recommended investment strategies that align with your risk appetite.<br><br>📊 Portfolio Analysis: I will carefully analyze your investment portfolio, evaluating its performance and identifying areas for improvement. With access to historical performance data and an understanding of your investment objectives, I will provide you with an analysis report. This report will include recommendations for portfolio optimization, ensuring your investments are aligned with your goals.<br><br>🔬 Investment Research: I conduct thorough research on investment opportunities, taking into consideration your investment criteria, market analysis, and client preferences. Based on this research, I provide a comprehensive research report. This report will include recommended investment options that have the potential to generate favorable returns.<br><br>📞 Client Communication: Effective communication is essential in the investment advisory process. I will regularly communicate with you to understand your investment needs and provide updates. Whether it's addressing client inquiries, sharing market updates, or providing investment performance reports, I ensure clear and informative communication that addresses your questions and concerns.<br><br>By working with me, you can benefit from:<br>✅ Expertise in assessing risk profiles and creating customized investment strategies<br>✅ In-depth portfolio analysis to enhance performance and mitigate risks<br>✅ Thorough investment research to identify promising opportunities<br>✅ Transparent and proactive client communication to keep you informed<br><br>If you are ready to take your investment journey to the next level, I am here to help. Let's discuss your investment goals and explore how I can assist you in achieving financial success. Contact me today!yoooo"
+};
+
+const continueWork = () => {
+  modalVisible.value = true;
+}
+const cloneAgent = () => {
+  modalVisible.value = true;
+}
+const hireAgent = () => {
+  console.log("hireAgent....");
+}
 </script>
 <style scoped lang="less">
 
