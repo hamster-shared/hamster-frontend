cc7407e11a1898d6840199f965fbc8a3083ca1ef
Dennisic\Chen <cgyforwork@foxmail.com>
2023-12-06 15:03:39 +0800

    feat: save code

--- src/views/aiAgent/home/agentData.ts
+++ src/views/aiAgent/home/agentData.ts
@@ -6,19 +6,19 @@ export const agentList = [
   {
     chatId: uuidv4(),//uuid
     userId: userInfo.id,
-    nickname: userInfo.username,
-    recordType: 0,// 0:NFT；1:Developer
-    logo: 'NFT-Expert.png',
-    position: 'NFT Expert',
-    description: 'NFT design、Contract development、NFT minting、Ethereum、Solana…',
-  },
-  {
-    chatId: uuidv4(),//uuid
-    userId: userInfo.id,
-    nickname: userInfo.username,
+    nickname: 'Richard Feynman',
     recordType: 1,// 0:NFT；1:Developer
     logo: 'Dapp-Developer.png',
     position: 'Dapp Developer',
     description: 'Dapp development、Smart contract development、Front-end integration、token creation、Deployment、Testing and debugging',
+  },
+  {
+    chatId: uuidv4(),//uuid
+    userId: userInfo.id,
+    nickname: 'Ada Lovelace',
+    recordType: 0,// 0:NFT；1:Developer
+    logo: 'NFT-Expert.png',
+    position: 'NFT Expert',
+    description: 'NFT design、Contract development、NFT minting、Ethereum、Solana…',
   }
 ];
